c87c1bd493226f758cd95ad279f493a7d2c9ff70
Wojciech Rzemieniuk <wojciechrzemieniuk@gmail.com>
2016-11-27 19:35:40 +0100

    dodano golibrode

---
+++ 4  synchronizacja/5 golibroda/Zadanie5.java
@@ -0,0 +1,112 @@
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Poczekalnia {
+	private final int MAX;
+
+	public Poczekalnia(int MAX) {
+		this.MAX = MAX;
+	}
+
+	ArrayList<String> poczekalnia = new ArrayList<String>();
+
+	synchronized boolean sprawdz() {
+		return poczekalnia.isEmpty();
+	}
+
+	synchronized void usiadz() {
+		if (poczekalnia.size() != MAX) {
+			if (poczekalnia.isEmpty()) {
+				System.out.println("Budze fryzjera");
+				notify();
+			}
+
+			poczekalnia.add("Klient" + poczekalnia.size());
+			System.out.println("Klient siada, stan poczekalni: " + poczekalnia);
+
+		}
+
+		else if (poczekalnia.size() == MAX) {
+			System.out.println("Poczekalnia pelna. Klient rezygnuje");
+		}
+	}
+
+	synchronized void spij() {
+		try {
+			while (poczekalnia.isEmpty()) {
+				System.out.println("Fryzjer idzie spac");
+				wait();
+			}
+		} catch (InterruptedException e) {
+		}
+	}
+
+	void strzyz() {
+		try {
+			poczekalnia.remove(0);
+			System.out.println("Klient strzyzony, stan poczekalni: " + poczekalnia);
+			Thread.sleep((int) (Math.random() * (5000 - 2000) + 2000));
+
+		} catch (InterruptedException e) {
+		}
+
+	}
+
+}
+
+class Golibroda implements Runnable {
+	private Poczekalnia poczekalnia;
+	private boolean pustaPoczekalnia = true;
+
+	Golibroda(Poczekalnia poczekalnia) {
+		this.poczekalnia = poczekalnia;
+		new Thread(this, "Klient").start();
+	}
+
+	public void run() {
+
+		while (true) {
+			pustaPoczekalnia = poczekalnia.sprawdz();
+
+			if (pustaPoczekalnia == true) {
+				poczekalnia.spij();
+			}
+
+			if (pustaPoczekalnia == false) {
+				poczekalnia.strzyz();
+			}
+		}
+	}
+}
+
+class Klient implements Runnable {
+	private Poczekalnia poczekalnia;
+
+	Klient(Poczekalnia poczekalnia) {
+		this.poczekalnia = poczekalnia;
+		new Thread(this, "Klient").start();
+	}
+
+	public void run() {
+
+		try {
+			while (true) {
+				poczekalnia.usiadz();
+				Thread.sleep((int) (Math.random() * (5000 - 2000 + 1000) + 2000));
+			}
+		} catch (InterruptedException ex) {
+		}
+
+	}
+}
+
+public class Zadanie5 {
+	public static void main(String[] args) {
+		Poczekalnia poczekalnia = new Poczekalnia(10);
+
+		new Golibroda(poczekalnia);
+		new Klient(poczekalnia);
+	}
+}
--- 4  synchronizacja/zadanie1 (zadanie 4 juz zsynchronizowane)/Zadanie4.java
+++ 4  synchronizacja/zadanie1 (zadanie 4 juz zsynchronizowane)/Zadanie4.java
@@ -1,51 +1,80 @@
-class Watek extends Thread {
-private String napis = "";
 
-public Watek(String tekst, int liczbaTabulacji) {
-for (int i = 0; i < liczbaTabulacji; i++) {
-napis += "\t";
-}
-napis = napis + tekst;
+class Pojemnik {
+	private int n;
+	private boolean pusty = true;
+
+	synchronized int get() {
+		if (pusty) {
+			try {
+				System.out.println("Konsument: czekam na produkcjê...");
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		System.out.println("Konsumpcja: " + n);
+		pusty = true;
+		notify();
+		return n;
+	}
+
+	synchronized void put(int n) {
+		if (!pusty) {
+			try {
+				System.out.println("Producent: czekam na konsumpcjê...");
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		this.n = n;
+		System.out.println("Produkcja:  " + n);
+		pusty = false;
+		notify();
+	}
 }
 
-public void run() {
+class Producent implements Runnable {
+	private Pojemnik p;
 
-synchronized (System.out) {
+	Producent(Pojemnik p) {
+		this.p = p;
+		new Thread(this, "Producent").start();
+	}
 
-for (int i = 1; i <= 100; i++) {
-for (int x = 0; x < napis.length(); x++)
-System.out.print(napis.charAt(x));
-System.out.println(i);
-yield();
-// try {
-// sleep(1000);
-// } catch(InterruptedException e) {}
+	public void run() {
+		for (int i = 1; i <= 10; i++) {
+			p.put(i);
+			try {
+				Thread.sleep((int) (Math.random() * 1000));
+			} catch (InterruptedException e) {
+			}
+		}
+	}
 }
-}
-}
+
+class Konsument implements Runnable {
+	private Pojemnik p;
+
+	Konsument(Pojemnik p) {
+		this.p = p;
+		new Thread(this, "Konsument").start();
+	}
+
+	public void run() {
+		for (int i = 1; i <= 10; i++) {
+			p.get();
+			try {
+				Thread.sleep((int) (Math.random() * 1000));
+			} catch (InterruptedException e) {
+			}
+		}
+	}
 }
 
 public class Zadanie4 {
-public static void main(String[] args) {
-new Watek("Marek", 1).start();
-new Watek("Kasia", 2).start();
-new Watek("Andrzej", 3).start();
-new Watek("Natalia", 4).start();
-}
-}
-public class Zadanie3 {
+	public static void main(String args[]) {
+		Pojemnik p = new Pojemnik();
+		new Producent(p);
+		new Konsument(p);
 
-public static void main (String[] args ) {
-Schowek schowek = new Schowek();
-
-System.out.println(schowek);
-
-new Zmiennik(schowek).start();
-new Zmiennik(schowek).start();
-new Zmiennik(schowek).start();
-new Zmiennik(schowek).start();
-
-System.out.println(schowek);
-
-}
+	}
 }
